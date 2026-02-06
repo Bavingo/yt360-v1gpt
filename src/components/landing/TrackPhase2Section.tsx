c20d40f59d48af4458a264cbd0e8264d3d1938d3
Bavingo <escolayt@gmail.com>
2026-02-05 21:47:56 -0300

    Update TrackPhase2Section.tsx

--- src/components/landing/TrackPhase2Section.tsx
+++ src/components/landing/TrackPhase2Section.tsx
@@ -18,7 +18,7 @@ const TrackPhase2Section = () => {
     <section className="py-20 md:py-32 bg-foreground relative overflow-hidden">
       {/* Animated background */}
       <div className="absolute inset-0">
-        <div className="absolute top-0 left-0 w-[600px] h-[600px] bg-primary/20 rounded-full blur-[120px]" />
+        <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-primary/20 rounded-full blur-[120px]" />
         <div className="absolute bottom-0 right-0 w-[400px] h-[400px] bg-primary/10 rounded-full blur-[100px]" />
       </div>
       
